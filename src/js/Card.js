c4f544cad7b4f2988b4bbc4fcaebc08dbe79c81b
Maggie Love <maggie.love1991@gmail.com>
2023-07-24 18:55:49 -0400

    Adds description placeholders to cards

--- src/js/Card.js
+++ src/js/Card.js
@@ -7,9 +7,12 @@ export default class Card extends React.Component {
     constructor(props) {
         super(props);
         this.handleClick = this.handleClick.bind(this);
+        this.handleMouseEnter = this.handleMouseEnter.bind(this);
+        this.handleMouseLeave = this.handleMouseLeave.bind(this);
 
         this.state = {
-            flipped: false
+            flipped: false,
+            descriptionHidden: true
         }
     }
 
@@ -19,9 +22,20 @@ export default class Card extends React.Component {
         });
     }
 
+    handleMouseEnter() {
+      this.setState({ descriptionHidden: false });
+    }
+
+    handleMouseLeave() {
+      this.setState({ descriptionHidden: true });
+    }
+
     render() {
         const { data } = this.props;
         const cardClass = classNames('card', { 'flipped': this.state.flipped });
+        const cardInfoClass = classNames('description', {
+          'hidden': this.state.descriptionHidden });
+        const description = { __html: `<p>${data.description}</p>` };
 
         return (
             <div className={cardClass} onClick={this.handleClick}>
@@ -30,12 +44,15 @@ export default class Card extends React.Component {
                       <img src={require('../images/card-top-view.png')}
                     alt={data.name} height={362} width={200} />
                     </div>
-                    <div className='card-back'>
+                    <div className='card-back' onMouseEnter={this.handleMouseEnter}
+                      onMouseLeave={this.handleMouseLeave}>
                         {data &&
                           <img src={require(`../${data.image}`)}
                           alt={data.name} height={362} width={200} />
                         }
                     </div>
+                    <div className={cardInfoClass}
+                      dangerouslySetInnerHTML={description} />
                 </div>
             </div>
         );
