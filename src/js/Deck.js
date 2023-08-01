852ffd266cadeb216f5f77bebd58f508126c974c
Maggie Love <maggie.love1991@gmail.com>
2023-08-01 15:35:32 -0400

    Adds typewriter welcome, gradient, WIP deck; broken begin functionality

--- src/js/Deck.js
+++ src/js/Deck.js
@@ -1,15 +1,39 @@
 import '../css/Deck.css';
+import TypeWriterEffect from "react-typewriter-effect";
 
 const Deck = ({ onRotate }) => {
   const text = { __html: `<p>Welcome. Please consider your intention for this
     reading. When you're ready, click here to begin.</p>` };
 
+  const handleClick = () => {
+    console.log(`clicked welcome txt`);
+    // add class to animate deck
+    // could make it a promise, so that when the animation is complete, it calls onRotate
+    // or i could move all card logic to the same component, either deck or app...?
+  }
+
   return (
     <div className="deck-wrapper">
-      <img src={require('../images/deck-sideways.png')} alt="tarot deck"
+      <img className="deck" src={require('../images/deck-sideways.png')} alt="tarot deck"
       width="500" height="347" />
-      <div className="welcome-text" dangerouslySetInnerHTML={text}
-        onClick={onRotate} />
+      <div className="welcome-text" onClick={handleClick}>
+      <TypeWriterEffect
+        textStyle={{
+          fontFamily: 'Lumanosimo',
+          color: '#f2f2f2',
+          fontSize: '21px'
+        }}
+        startDelay={1000}
+        cursorColor="white"
+        multiText={[
+          'Welcome.',
+          'Please consider your intention for this reading.',
+          'When you\'re ready, click here to begin.'
+        ]}
+        multiTextDelay={2000}
+        typeSpeed={100}
+      />
+      </div>
     </div>
   )
 }
